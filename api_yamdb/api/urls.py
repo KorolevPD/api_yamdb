637584a1f464142e464b42474ec230eef2a1ad6a
Pavel Korolev <p.koror56@gmail.com>
2025-03-18 23:14:26 +0500

    Настроил JWT и url для эндпоинтов auth и users

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import UserProfileViewSet, UserViewSet
+from .views import UserSignupTokenViewSet, UserViewSet
 
 
 router = SimpleRouter()
+router.register(r'auth', UserSignupTokenViewSet, 'users')
+
+# TODO Нужно объединить эти эндпоинты
 router.register(r'users', UserViewSet, 'users')
-router.register(r'users/me', UserProfileViewSet, 'user-profile')
+router.register(r'users/me', UserViewSet, 'user-profile')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
