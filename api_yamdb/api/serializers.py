7fb7307d98cfb152be19deb0c244deaeb7e6936e
Pavel Korolev <p.koror56@gmail.com>
2025-03-18 16:58:10 +0500

    Поменял двойные кавычки на одинарные

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -8,7 +8,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ("id", "title", "text", "author", "score", "pub_date")
-        read_only_fields = ("title",)
+        fields = ('id', 'title', 'text', 'author', 'score', 'pub_date')
+        read_only_fields = ('title',)
 
 
