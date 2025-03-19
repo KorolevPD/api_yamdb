cf338f2820706986aae6577ade41b698d568cbbf
Pavel Korolev <p.koror56@gmail.com>
2025-03-19 16:00:46 +0500

    Поправил эндпоинты users + добавил categories и genres

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,15 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import UserSignupTokenViewSet, UserViewSet, ReviewViewSet
+from .views import (UserSignupTokenViewSet, UserViewSet, ReviewViewSet,
+                    CategoryViewSet, GenreViewSet)
 
 router = SimpleRouter()
-router.register(r'auth', UserSignupTokenViewSet, 'users')
-
-# TODO Нужно объединить эти эндпоинты
+router.register(r'auth', UserSignupTokenViewSet, 'auth')
 router.register(r'users', UserViewSet, 'users')
-router.register(r'users/me', UserViewSet, 'user-profile')
-router.register(r'titles/(?P<title_pk>\d+)/reviews', ReviewViewSet, basename='reviews')
+router.register(r'categories', CategoryViewSet, 'categories')
+router.register(r'genres', GenreViewSet, 'genres')
+router.register(r'titles/(?P<title_pk>\d+)/reviews', ReviewViewSet,
+                basename='reviews')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
