d2e025dc8ef45fe5ad1d8e32570c12181e762611
Абильханов Рауан Болатулы <rauan.abilkhanov@kaspi.kz>
2025-03-19 15:20:57 +0500

    CommentViewSet,CommentSerializer

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import UserSignupTokenViewSet, UserViewSet, ReviewViewSet
+from .views import UserSignupTokenViewSet, UserViewSet, ReviewViewSet, CommentViewSet
 
 router = SimpleRouter()
 router.register(r'auth', UserSignupTokenViewSet, 'users')
@@ -9,7 +9,8 @@ router.register(r'auth', UserSignupTokenViewSet, 'users')
 # TODO Нужно объединить эти эндпоинты
 router.register(r'users', UserViewSet, 'users')
 router.register(r'users/me', UserViewSet, 'user-profile')
-router.register(r'titles/(?P<title_pk>\d+)/reviews', ReviewViewSet, basename='reviews')
+router.register(r'titles/(?P<title_pk>\d+)/reviews', ReviewViewSet, 'reviews')
+router.register(r'titles/(?P<title_pk>\d+)/reviews/(?P<review_pk>\d+)/comments', CommentViewSet, 'comments')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
