2350cafebb4557250b9c2c3b0e1f35b16f7ab7d6
Pavel Korolev <p.koror56@gmail.com>
2025-03-20 12:05:08 +0500

    Изменил пермишен IsAdministrator

--- api_yamdb/permissions.py
+++ api_yamdb/permissions.py
@@ -15,4 +15,4 @@ class IsAuthorOrModeratorOrAdmin(permissions.BasePermission):
 class IsAdministrator(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_superuser
-                or request.user.role in ['moderator', 'admin'])
+                or request.user.role in ('admin',))
