3caad2664b7fa12e1e6f685f24a89f1a140d14ec
Pavel Korolev <p.koror56@gmail.com>
2025-03-20 12:06:34 +0500

    Поправил импорты в Url

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,18 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import (UserSignupTokenViewSet, UserViewSet, ReviewViewSet,
-                    CategoryViewSet, GenreViewSet, CommentViewSet)
+from . import views
 
 
 router = SimpleRouter()
-router.register(r'auth', UserSignupTokenViewSet, 'auth')
-router.register(r'users', UserViewSet, 'users')
-router.register(r'categories', CategoryViewSet, 'categories')
-router.register(r'genres', GenreViewSet, 'genres')
-router.register(r'titles/(?P<title_pk>\d+)/reviews', ReviewViewSet,
+router.register(r'auth', views.UserSignupTokenViewSet, 'auth')
+router.register(r'users', views.UserViewSet, 'users')
+router.register(r'categories', views.CategoryViewSet, 'categories')
+router.register(r'genres', views.GenreViewSet, 'genres')
+router.register(r'titles', views.TitleViewSet, 'titles')
+router.register(r'titles/(?P<title_pk>\d+)/reviews', views.ReviewViewSet,
                 basename='reviews')
-router.register(r'titles/(?P<title_pk>\d+)/reviews/(?P<review_pk>\d+)/comments', CommentViewSet, 'comments')
+router.register(r'titles/(?P<title_pk>\d+)/reviews/(?P<review_pk>\d+)/comments', views.CommentViewSet, 'comments')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
