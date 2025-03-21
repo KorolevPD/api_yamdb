7bbcc6a6304bc03433484ce71e5df2e817e3c22f
Михаил Садчиков <msadchikov89@mail.ru>
2025-03-21 21:59:29 +0500

    Доработал авторизацию

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -6,7 +6,9 @@ from reviews.models import Review, User, Category, Genre, Comment, Title
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.CharField(
+        required=True,
         allow_blank=False,
+        max_length=254,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
